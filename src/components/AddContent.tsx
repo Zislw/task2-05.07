4ea80ce82d46b92ee0d1cdf12a2cdde552519ba9
Zislw <w05567716@gmail.com>
2022-07-05 22:32:01 +0300

    second commit

--- src/components/AddContent.tsx
+++ src/components/AddContent.tsx
@@ -3,20 +3,24 @@ import "./AddContent.css"
 import cont from"../content.json"
 export class AddContent extends React.Component{
    state={
-    con:""
+    jsonArray:[{display:"",url:""}]
+    
    }
    getContent=()=>{
-        let x=cont.content
-        this.setState({con:x})
-
+        let d=cont
+        this.setState({jsonArray:[...d]})
    }
    
    
     render() {
+        let i=0
     return(
+       
         <div className="wrap">
         <h1>Welcome to my new site</h1>
-        <div className="content">{this.state.con}</div>
+       
+        <div className="content">
+            {this.state.jsonArray.map(item=>{return <div key={i++}><label>{item.display}{item.url}</label></div>})}</div>
         <input type="button" value="click me to get content" onClick={()=>{ this.getContent()}} />
         </div>
     )
